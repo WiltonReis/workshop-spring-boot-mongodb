8d975cbd7c13dad8c30ac4146e8f2d2e832d48f9
Wilton Reis <wiltonfilho0825@gmail.com>
2025-05-20 16:13:53 -0300

    Funcao insert user

--- src/main/java/com/wiltonreis/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wiltonreis/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.wiltonreis.workshopmongo.resources;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wiltonreis.workshopmongo.dto.UserDTO;
 import com.wiltonreis.workshopmongo.entities.User;
@@ -33,4 +36,13 @@ public class UserResource {
 		UserDTO user = new UserDTO(service.findById(id));
 		return ResponseEntity.ok().body(user);
 	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody User user){
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(new UserDTO(user));
+	}
+	
 }
+
